5b2b4bdcc23c5abd7a04c952144ba4fd3145ace6
Aman Rajput <amanrajput7055148434@gmail.com>
2025-04-15 21:11:35 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/Demo-Project",
+});
